530c658ade389cb976d72c73c4f3bf8b3833c05c
Miner3210 <patsakorn.mark@mail.kmutt.ac.th>
2021-04-16 15:37:02 +0700

    [62130500065] Rework Project and Add Index page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home  mx-10">
+    <div class="grid grid-cols-8 grid-rows-5 font-custom">
+      <div class="col-start-3 col-end-7">
+       <h1 class=" font-medium md:text-center text-5xl mt-20 p-2 border-b-4 border-black">Admin Setting Room</h1>
+      </div>
+      <div class="col-start-4 col-end-6 row-start-4 self-center ">
+        <p class=" p-6 text-center">Click to Admin menu</p>
+        <router-link to="/main" class="md:p-6 md:px-28 bg-black text-white text-2xl shadow-xl hover:bg-white hover:text-black rounded-full">Menu</router-link>
+      </div>
+       </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
